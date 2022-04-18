d4fd4c263596fe9d7e67b66eaddfde78e0238e90
Rukin Danil <rukin2018d@yandex.ru>
2022-04-18 13:56:50 +0700

    Работает обновление

--- MineralsApp.Client/Controllers/MineralsController.cs
+++ MineralsApp.Client/Controllers/MineralsController.cs
@@ -7,6 +7,7 @@ using MineralsApp.DataAccessLayer.Repositories;
 using MineralsApp.DataAccessLayer.Repositories.Interfaces;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 
 namespace MineralsApp.Client.Controllers
 {
@@ -46,17 +47,34 @@ namespace MineralsApp.Client.Controllers
             var mineral = _mineralRepository.Get(id);
             if (mineral == null)
                 return NotFound(nameof(mineral));
-            return View("EditMineral", new UpdateMineralViewModel(mineral, _mineralRepository));
+            UpdateMineralViewModel model = new UpdateMineralViewModel()
+            {
+                MineralId = mineral.Id,
+                Description = mineral.Description,
+                Name = mineral.Name,
+                PathToImage = mineral.PathToImage,
+                Fields = GetFieldsProperty(mineral),
+                Territories = GetTerritoriesProperty(),
+                Researchers = GetResearchersProperty(mineral),
+                Publications = GetPublicationsProperty(mineral),
+            };
+            return View("EditMineral", model);
         }
 
         [HttpPost]
-        public IActionResult Update(Mineral mineral)
+        public IActionResult Update(UpdateMineralViewModel model)
         {
-            if (mineral == null)
+            if (model == null)
                 return NotFound();
+            Mineral mineral = _mineralRepository.Get(model.MineralId);
+            if (mineral == null)
+                return NotFound(nameof(mineral));
+            UpdateMineral(mineral, model);
             _mineralRepository.Save(mineral);
             return View("ListOfMinerals", new ListOfMineralsViewModel(_mineralRepository));
         }
+
+        
 
         [HttpDelete]
         public IActionResult Delete(int id)
@@ -64,5 +82,168 @@ namespace MineralsApp.Client.Controllers
             _mineralRepository.Delete(id);
             return View("ListOfMinerals", new ListOfMineralsViewModel(_mineralRepository));
         }
+
+        private void UpdateMineral(Mineral mineral, UpdateMineralViewModel model)
+        {
+            mineral.Id = model.MineralId;
+            mineral.Name = model.Name;
+            mineral.Description = model.Description;
+            SetFields(model.Fields, mineral);
+            SetResearchers(model.Researchers, mineral);
+            SetTerritories(model.Territories, mineral);
+        }
+
+        #region private
+        private string CollectionToString<T>(ICollection<T> collection)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in collection)
+            {
+                builder.Append($"{item}, ");
+            }
+            string result = builder.ToString();
+            return result.Substring(0, result.Length - 2);
+        }
+
+        private ICollection<Publication> GetPublications(ICollection<PublicationDescribesMineral> publicationDescribesMinerals)
+        {
+            List<Publication> publications = new List<Publication>();
+            foreach (var publication in publicationDescribesMinerals)
+            {
+                publications.Add(publication.Publication);
+            }
+            return publications;
+        }
+
+        private string GetPublicationsProperty(Mineral mineral) =>
+            CollectionToString(GetPublications(mineral.PublicationDescribesMineral));
+
+        private ICollection<Ore> GetOres(ICollection<OreHasMineral> oreHasMinerals)
+        {
+            List<Ore> ores = new List<Ore>();
+            foreach (var item in oreHasMinerals)
+            {
+                ores.Add(item.Ore);
+            }
+            return ores;
+        }
+
+        private ICollection<Field> GetFields(ICollection<FieldHasMineral> fieldHasMinerals)
+        {
+            List<Field> fields = new List<Field>();
+            foreach (var item in fieldHasMinerals)
+            {
+                fields.Add(item.Field);
+            }
+            return fields;
+        }
+
+        private ICollection<Researcher> GetResearchers(Mineral mineral)
+        {
+            ICollection<Publication> publications = GetPublications(mineral.PublicationDescribesMineral);
+            List<Researcher> researchers = new List<Researcher>();
+            foreach (var item in publications)
+            {
+                foreach (var researcherHasPublication in item.ResearcherHasPublication)
+                {
+                    if (!researchers.Contains(researcherHasPublication.Researcher))
+                    {
+                        researchers.Add(researcherHasPublication.Researcher);
+                    }
+
+                }
+            }
+            return researchers;
+        }
+
+        private void SetFields(string value, Mineral mineral)
+        {
+            ICollection<Field> realFields = GetFields(mineral.FieldHasMinerals);
+            string[] fields = value.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
+            bool shouldAdd;
+            foreach (var item in fields)
+            {
+                shouldAdd = true;
+                foreach (var field in realFields)
+                {
+                    if (field.Name == item)
+                    {
+                        shouldAdd = false;
+                        break;
+                    }
+                }
+                if (shouldAdd)
+                {
+                    mineral.FieldHasMinerals.Add(new FieldHasMineral()
+                    {
+                        Field = new Field() { Name = item },
+                        Mineral = mineral,
+                    });
+                }
+            }
+        }
+
+        private void SetResearchers(string value, Mineral mineral)
+        {
+            string[] researchers = value.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
+            string[] names;
+            bool shouldAdd;
+            ICollection<Researcher> realResearchers = GetResearchers(mineral);
+            foreach (var item in researchers)
+            {
+                shouldAdd = true;
+                names = item.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+                foreach (var realResearcher in realResearchers)
+                {
+                    if (names[0] == realResearcher.FirstName
+                        && names[1] == realResearcher.LastName
+                        && names[2] == realResearcher.Patronymic)
+                    {
+                        shouldAdd = false;
+                        break;
+                    }
+                }
+                if (shouldAdd)
+                {
+                    Researcher researcher = new Researcher()
+                    {
+                        FirstName = names[0],
+                        LastName = names[1],
+                        Patronymic = names[2],
+                    };
+                    Publication publication = new Publication()
+                    {
+                        Name = $"{mineral.Name} by {researcher}",
+                        ResearcherHasPublication = new List<ResearcherHasPublication>()
+                    };
+                    publication.ResearcherHasPublication.Add(new ResearcherHasPublication() { Publication = publication, Researcher = researcher });
+                    mineral.PublicationDescribesMineral.Add(new PublicationDescribesMineral()
+                    {
+                        Mineral = mineral,
+                        Publication = publication,
+                    });
+                }
+            }
+        }
+
+        private void SetTerritories(string value, Mineral mineral)
+        {
+            mineral.FieldHasMinerals.Add(new FieldHasMineral()
+            {
+                Mineral = mineral,
+                Field = new Field()
+                {
+                    Name = $"Месторождение минерала {mineral.Id}",
+                    Territory = new Territory()
+                }
+            });
+        }
+
+        private string GetFieldsProperty(Mineral mineral) => CollectionToString(GetFields(mineral.FieldHasMinerals));
+
+        private string GetResearchersProperty(Mineral mineral) => CollectionToString(GetResearchers(mineral));
+
+        private string GetTerritoriesProperty() => "Территория 1, Территория 2, Территория 3";
+        #endregion
     }
 }
--- MineralsApp.Client/ViewModels/UpdateMineralViewModel.cs
+++ MineralsApp.Client/ViewModels/UpdateMineralViewModel.cs
@@ -5,171 +5,18 @@ using System.Text;
 
 namespace MineralsApp.Client.ViewModels
 {
-    public class UpdateMineralViewModel : SaveMineralViewModel
+    public class UpdateMineralViewModel
     {
-        private Mineral _mineral;
-        private IRepository<Mineral> _mineralRepository;
-        public UpdateMineralViewModel(Mineral mineral, IRepository<Mineral> repository)
-        {
-            _mineral = mineral;
-            _mineralRepository = repository;
-        }
-        public int MineralId { get => _mineral.Id; }
-        public override string Name { get => _mineral.Name; set => _mineral.Name = value; }
-        public override string Description { get => _mineral.Description; set => _mineral.Description = value; }
-        public override string PathToImage { get => _mineral.PathToImage; set => _mineral.PathToImage = value; }
+        public int MineralId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PathToImage { get; set; }
         public string Publications { get; set; }
-        public override string Fields
-        {
-            get => CollectionToString(GetFields(_mineral.FieldHasMinerals));
-            set
-            {
-                ICollection<Field> realFields = GetFields(_mineral.FieldHasMinerals);
-                string[] fields = value.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
-                bool shouldAdd;
-                foreach (var item in fields)
-                {
-                    shouldAdd = true;
-                    foreach (var field in realFields)
-                    {
-                        if (field.Name == item)
-                        {
-                            shouldAdd = false;
-                            break;
-                        }                            
-                    }
-                    if (shouldAdd)
-                    {
-                        _mineral.FieldHasMinerals.Add(new FieldHasMineral() 
-                        { 
-                            Field = new Field() { Name = item },
-                            Mineral = _mineral,
-                        });
-                    }
-                }
-            }
-        }
+        public string Fields { get; set; }
 
-        public override string Researchers
-        {
-            get => CollectionToString(GetResearchers());
-            set
-            {
-                string[] researchers = value.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
-                string[] names;
-                bool shouldAdd;
-                ICollection<Researcher> realResearchers = GetResearchers();
-                foreach (var item in researchers)
-                {
-                    shouldAdd = true;
-                    names = item.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
-                    foreach (var realResearcher in realResearchers)
-                    {
-                        if (names[0] == realResearcher.FirstName
-                            && names[1] == realResearcher.LastName
-                            && names[2] == realResearcher.Patronymic)
-                        {
-                            shouldAdd = false;
-                            break;
-                        }
-                    }
-                    if (shouldAdd)
-                    {
-                        Researcher researcher = new Researcher()
-                        {
-                            FirstName = names[0],
-                            LastName = names[1],
-                            Patronymic = names[2],                           
-                        };
-                        Publication publication = new Publication()
-                        {
-                            Name = $"{_mineral.Name} by {researcher}",
-                            ResearcherHasPublication = new List<ResearcherHasPublication>()
-                        };
-                        publication.ResearcherHasPublication.Add(new ResearcherHasPublication() { Publication = publication, Researcher = researcher });
-                        _mineral.PublicationDescribesMineral.Add(new PublicationDescribesMineral()
-                        {
-                            Mineral = _mineral,
-                            Publication = publication,
-                        });
-                    }
-                }
-            }
-        }
-        public override string Territories
-        {
-            get => "Территория 1, Территория 2, Территория 3";
-            set
-            {
-                _mineral.FieldHasMinerals.Add(new FieldHasMineral()
-                {
-                    Mineral = _mineral,
-                    Field = new Field()
-                    {
-                        Name = $"Месторождение минерала {_mineral.Id}",
-                        Territory = new Territory()
-                    }
-                });
-            }
-        }
+        public string Researchers { get; set; }
+        public string Territories { get; set; }
 
-        private string CollectionToString<T>(ICollection<T> collection)
-        {
-            StringBuilder builder = new StringBuilder();
-            foreach (var item in collection)
-            {
-                builder.Append($"{item}, ");
-            }
-            string result = builder.ToString();
-            return result.Substring(0, result.Length - 2);
-        }
-
-        private ICollection<Publication> GetPublications(ICollection<PublicationDescribesMineral> publicationDescribesMinerals)
-        {
-            List<Publication> publications = new List<Publication>();
-            foreach (var publication in publicationDescribesMinerals)
-            {
-                publications.Add(publication.Publication);
-            }
-            return publications;
-        }
-
-        private ICollection<Ore> GetOres(ICollection<OreHasMineral> oreHasMinerals)
-        {
-            List<Ore> ores = new List<Ore>();
-            foreach (var item in oreHasMinerals)
-            {
-                ores.Add(item.Ore);
-            }
-            return ores;
-        }
-
-        private ICollection<Field> GetFields(ICollection<FieldHasMineral> fieldHasMinerals)
-        {
-            List<Field> fields = new List<Field>();
-            foreach (var item in fieldHasMinerals)
-            {
-                fields.Add(item.Field);
-            }
-            return fields;
-        }
-
-        private ICollection<Researcher> GetResearchers()
-        {
-            ICollection<Publication> publications = GetPublications(_mineral.PublicationDescribesMineral);
-            List<Researcher> researchers = new List<Researcher>();
-            foreach (var item in publications)
-            {
-                foreach (var researcherHasPublication in item.ResearcherHasPublication)
-                {
-                    if (!researchers.Contains(researcherHasPublication.Researcher))
-                    {
-                        researchers.Add(researcherHasPublication.Researcher);
-                    }
-
-                }
-            }
-            return researchers;
-        }
+        
     }
 }
